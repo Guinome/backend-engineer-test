205833568e6c68d196345b202a2e23b8f7df585d
Guillaume ORSONI <guillaumeorsoni@gmail.com>
2018-09-13 16:37:38 +0200

    working commit

--- exercise.js
+++ exercise.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const _ = require('lodash');
+const computedSkillsService = require('./services/computedSkills');
 
-const freelancerFile = './exercise/freelancer.json'
+// const freelancerFile = './exercise/freelancer.json';
+const freelancerFile = './examples/freelancer.json';
 
 if (!fs.existsSync(freelancerFile)) {
 	console.log('File does not exists');
@@ -10,12 +13,15 @@ let freelancer = fs.readFileSync(freelancerFile, 'utf8');
 
 freelancer = JSON.parse(freelancer);
 
-var freelanceId = freelancer.freelance.id;
-
-var jsonReturn = {
-	"id": 			  freelancer.freelance.id,
-	"computedSkills": []
-}
+//order professional experiences by startdate
+var experiences    = computedSkillsService.getExperiencesOrderedByStartDate(freelancer.freelance)
+var computedSkills = computedSkillsService.getComputedSkills(experiences);
 // compute all skills duration
-
+const jsonReturn = {
+		"freelance": {
+			"id": 			  freelancer.freelance.id,
+			"computedSkills": computedSkills
+		}
+	};
+console.log(JSON.stringify(jsonReturn));
 // output result
---
+++ services/computedSkills.js
@@ -0,0 +1,73 @@
+const _ = require('lodash');
+const moment = require('moment');
+module.exports = {
+    getExperiencesOrderedByStartDate(freelance){
+        return _.orderBy(freelance.professionalExperiences, ['startDate'], ['asc']);
+    },
+
+    getComputedSkills(experiences){
+        var totalSkills = [];
+
+        for (let index = 0; index < experiences.length; index++) {
+            var currentExperience         = experiences[index];
+            var currentExperienceDuration = this.getExperienceDuration(currentExperience);
+            // première iteration = initialisation des premiers skills
+            if(index === 0){
+                totalSkills = this.initFirstSkills(currentExperience.skills, currentExperienceDuration);
+            } else {
+                var durationToSubstract = this.getDurationToSubstract(experiences[index - 1], experiences[index]);
+    
+                currentExperience.skills.forEach(skill => {
+                    var skillIndex = this.getSkillIndex(totalSkills, skill);
+                    //If the skill doesn't exist, we create it
+                    if (skillIndex === -1) {
+                        var newSkill = skill;
+                        newSkill.durationInMonth = currentExperienceDuration;
+                        totalSkills.push(newSkill);
+                    } else{
+                        totalSkills[skillIndex].durationInMonth += currentExperienceDuration;
+                        var previousSkillIndex = this.getSkillIndex(experiences[index - 1].skills, skill);
+                        if(previousSkillIndex !== -1){
+                            totalSkills[skillIndex].durationInMonth -= durationToSubstract;
+                        }
+                    }
+                });
+            }
+        }
+
+        return totalSkills;
+    },
+
+    getExperienceDuration(experience) {
+        var startDate = moment(experience.startDate);
+        var endDate = moment(experience.endDate);
+
+        var duration = endDate.diff(startDate, 'month');
+        return duration;
+    },
+
+    initFirstSkills(skills, duration) {
+        var firstSkills = _.map(skills, function (skill) {
+            skill.durationInMonth = duration;
+            return skill;
+        });
+        return firstSkills;
+    },
+
+    getDurationToSubstract(previousExperience, currentExperience){
+        const previousExperienceEndDate = moment(previousExperience.endDate);
+        const currentExperienceStartDate = moment(currentExperience.startDate);
+        var durationToSubstract = 0;
+        //If there is overlap, we change the duration to substract
+        if (currentExperienceStartDate.isBefore(previousExperienceEndDate)) {
+            var durationToSubstract = previousExperienceEndDate.diff(currentExperienceStartDate, 'month');
+        }
+        return durationToSubstract;
+    },
+
+    getSkillIndex(skills, skillToFind){
+        return _.findIndex(skills, function (skill) { 
+            return skill.id == skillToFind.id;
+        });
+    }
+};
